af17eab1214f9c026a173d4bf0e0d4594bb9fc0b
Todd Malone <toddomalone@gmail.com>
2021-01-10 21:43:10 -0600

    added event listers to all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,21 @@ function startQuiz(){
     questionPane.attr("class","row");
 }
 
+function answerClicked(which){
+    console.log(which)
+    //load next question
+}
 
+
+//Click listers for buttons
 $("#startButton").on("click", startQuiz)
+
+// TODO - is there a better way to handle this?
+$("#answer-btn-1").on("click", function(){answerClicked(1)});
+$("#answer-btn-2").on("click", function(){answerClicked(2)})
+$("#answer-btn-3").on("click", function(){answerClicked(3)})
+$("#answer-btn-4").on("click", function(){answerClicked(4)})
+
 
 // var start = document.querySelector("#startButton");
 // start.addEventListener("click",startQuiz)
