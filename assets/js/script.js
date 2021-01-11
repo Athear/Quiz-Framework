5c1bd27165cb90d50b976edbb5e4e1662aa2d456
Todd Malone <toddomalone@gmail.com>
2021-01-10 21:26:12 -0600

    added initial jQuery to hide elements

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var questionPane =$("#question-pane");
+var greetingPane =$("greeting-pane");
+
+var currentClass = questionPane.attr("class");
+questionPane.attr("class",currentClass+" hidden");
+
+
+function startQuiz(){
+    console.log("i got called")
+    greetingPane.attr("class","hidden");
+    questionPane.attr("class","row");
+}
+
+
+// $("#startButton").on("click", startQuiz())
