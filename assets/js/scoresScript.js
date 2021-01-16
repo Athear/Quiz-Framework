ef189358f944d148456e4ffbee7c2d672001b35d
Todd Malone <toddomalone@gmail.com>
2021-01-15 21:29:09 -0600

    added buttons for highscores page. Back button functions

---
+++ assets/js/scoresScript.js
@@ -0,0 +1,12 @@
+
+
+//add listeners for buttons
+$("#back-button").on("click",function(){
+    console.log("back clicked");
+    window.location = '../../index.html'
+})
+
+$("#clear-button").on("click",function(){
+    
+
+})
