4435c808d55d805e26092afd46c488d19ca03ff0
Todd Malone <toddomalone@gmail.com>
2021-01-15 22:50:55 -0600

    added dynamic score element generation

--- assets/js/scoresScript.js
+++ assets/js/scoresScript.js
@@ -1,5 +1,25 @@
 
 
+var scoreStorage=(JSON.parse(localStorage.getItem("quizScores")));
+
+function makeScoreElement(){
+    var newRow = $("<div class=row>")
+    var frontBuffer = $('<div class="col-md-1">')
+    var backBuffer = $('<div class="col-md-1">')
+    var scoreCol = $('<div class="col-md-10"><p class="score-item">')
+
+    scoreCol.text("1. dummy score - 10")
+
+    newRow.append(frontBuffer);
+    newRow.append(scoreCol);
+    newRow.append(backBuffer);
+    $("#score-container").append(newRow);
+
+
+}
+
+makeScoreElement();
+
 //add listeners for buttons
 $("#back-button").on("click",function(){
     console.log("back clicked");
@@ -7,6 +27,6 @@ $("#back-button").on("click",function(){
 })
 
 $("#clear-button").on("click",function(){
-    
-
+    $("#score-container").empty();
+    localStorage.removeItem("quizScores");
 })
