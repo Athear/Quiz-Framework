475512d98b4a6f8c2b50f6a9db662a5ff675a8c3
Todd Malone <toddomalone@gmail.com>
2021-01-15 23:18:57 -0600

    submit button now directs to the high scores page.

--- assets/js/scoresScript.js
+++ assets/js/scoresScript.js
@@ -2,29 +2,29 @@
 
 var scoreStorage=(JSON.parse(localStorage.getItem("quizScores")));
 
+//if there are scores, iterate through them and create a new element to hold them
 if(scoreStorage){
-for(var i=0;i<scoreStorage.length;i++){
-    var scoreObj = scoreStorage[i];
-    var scoreText = i+1+". Name: "+scoreObj.name+" | Score: "+scoreObj.score
-    makeScoreElement(scoreText)
-}
+    for(var i=0;i<scoreStorage.length;i++){
+        var scoreObj = scoreStorage[i];
+        var scoreText = i+1+". Name: "+scoreObj.name+" | Score: "+scoreObj.score
+        makeScoreElement(scoreText)
+    }
 }
 
 function makeScoreElement(scoreText){
+    //form a new row with appropriate columns
     var newRow = $("<div class=row>")
     var frontBuffer = $('<div class="col-md-1">')
     var backBuffer = $('<div class="col-md-1">')
     var scoreCol = $('<div class="col-md-10"><p class="score-item">')
 
+    //add the given score
     scoreCol.text(this.scoreText)
-
 
     newRow.append(frontBuffer);
     newRow.append(scoreCol);
     newRow.append(backBuffer);
     $("#score-container").append(newRow);
-
-
 }
 
 
